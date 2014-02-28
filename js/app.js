d193cef88ca0e27169ed8817bb39a17de2253e2f
adeng21 <adeng21@gmail.com>
2014-02-28 12:03:02 -0500

    Guess the Number game

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-alert('Hey find me to know where to put the javascript');
+var yourName = prompt("Welcome to the Guess the Number game. What is your name?");
+
+var randomNumber = Math.floor(Math.random()*501);
+
+while (guess != randomNumber){
+
+var guess = prompt("Hello " + yourName + ', please guess a number between 0 and 500');
+
+  if( guess > randomNumber){
+    alert(yourName + ', your guess is too high, please try again');
+
+  }else if(guess < randomNumber){
+    alert(yourName + ', your guess is too low, please try again');
+  }
+  else{
+    alert("Congratulations! You've guessed the right number!");
+  }
+}
+
+
